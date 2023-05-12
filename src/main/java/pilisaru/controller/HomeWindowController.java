f47571b195ee5d049716475efdac03e99a7693a5
SavindaJayasekara <thantrige32@gmail.com>
2023-05-12 18:39:58 +0530

    employee crud created !

--- src/main/java/pilisaru/controller/HomeWindowController.java
+++ src/main/java/pilisaru/controller/HomeWindowController.java
@@ -1,9 +1,91 @@
 package pilisaru.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import pilisaru.dto.Employee;
+import pilisaru.model.EmployeeModel;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeWindowController {
-    public void btnUpdateOnAction(ActionEvent event) {
+    public ComboBox cmbNic;
+    public Button btnUpdate;
+    public TextField txtEmpName;
+    public TextField txtAddress;
+    public Button btnDelete;
 
+    @FXML
+    void initialize(){
+        setNicNUmbers();
+    }
+
+    private void setNicNUmbers() {
+        ObservableList nic= FXCollections.observableArrayList();
+        try {
+            List<String> nics=EmployeeModel.getNicNumbers();
+
+            for(String s : nics){
+                nic.add(s);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        cmbNic.setItems(nic);
+    }
+
+    public void btnUpdateOnAction(ActionEvent event) {
+        String nic =(String) cmbNic.getValue();
+        String address = txtAddress.getText();
+        String name = txtEmpName.getText();
+
+        try {
+            boolean isUpdate= EmployeeModel.update(new Employee(nic,address,name));
+
+            if (isUpdate){
+                new Alert(Alert.AlertType.CONFIRMATION,"Success !").show();
+            }else {
+                new Alert(Alert.AlertType.ERROR,"SQL Error !").show();
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void cmbNicOnAction(ActionEvent event) {
+        String nic = (String) cmbNic.getValue();
+
+        try {
+            Employee employee=EmployeeModel.getAll(nic);
+            txtEmpName.setText(employee.getName());
+            txtAddress.setText(employee.getAddress());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void btnDeleteOnAction(ActionEvent event) {
+        String nic = (String) cmbNic.getValue();
+
+        try {
+            boolean isDeleted=EmployeeModel.delete(nic);
+            if (isDeleted){
+                new Alert(Alert.AlertType.CONFIRMATION,"Success !").show();
+            }else {
+                System.out.println("SQL ERROR !");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        initialize();
     }
 }
--- src/main/java/pilisaru/model/EmployeeModel.java
+++ src/main/java/pilisaru/model/EmployeeModel.java
@@ -3,12 +3,47 @@ package pilisaru.model;
 import pilisaru.dto.Employee;
 import pilisaru.util.CrudUtil;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmployeeModel  {
     public static boolean save(Employee employee) throws SQLException {
         String sql="INSERT INTO employee(NIC,Name,Address)" +
                 "VALUES(?, ?, ?)";
         return CrudUtil.execute(sql,employee.getNic(),employee.getName(),employee.getAddress());
+    }
+
+    public static boolean update(Employee employee) throws SQLException {
+        String sql="UPDATE employee SET Name=? ,Address=?" +
+                "WHERE NIC=?";
+        return CrudUtil.execute(sql,employee.getName(),employee.getAddress(),employee.getNic());
+    }
+
+    public static List<String> getNicNumbers() throws SQLException {
+        List<String> nic=new ArrayList<>();
+        String sql="SELECT NIC FROM employee";
+        ResultSet resultSet=CrudUtil.execute(sql);
+        while (resultSet.next()) {
+            nic.add(resultSet.getString(1));
+        }
+        return nic;
+    }
+
+    public static Employee getAll(String nic) throws SQLException {
+        String sql="SELECT * FROM employee WHERE NIC=?";
+
+        ResultSet resultSet=CrudUtil.execute(sql,nic);
+
+        if (resultSet.next()){
+            return new Employee(nic,resultSet.getString(1),resultSet.getString(2));
+        }
+        return null;
+    }
+
+    public static boolean delete(String nic) throws SQLException {
+        String sql="DELETE FROM employee WHERE NIC=?";
+        return CrudUtil.execute(sql,nic);
     }
 }
