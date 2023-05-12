a88ea0b6a8b247f74dce4e9dce9a0e3539d9843b
SavindaJayasekara <thantrige32@gmail.com>
2023-05-12 10:36:07 +0530

    add ui logic !

--- src/main/java/pilisaru/controller/LoginController.java
+++ src/main/java/pilisaru/controller/LoginController.java
@@ -1,5 +1,19 @@
 package pilisaru.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.KeyEvent;
+
 public class LoginController {
 
+    public void txtPasswordOnAction(ActionEvent event) {
+    }
+
+    public void passOnActionKeyTyped(KeyEvent keyEvent) {
+    }
+
+    public void btnSingInOnAction(ActionEvent event) {
+    }
+
+    public void forgetsOnAction(ActionEvent event) {
+    }
 }
