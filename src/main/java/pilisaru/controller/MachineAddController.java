aa2acf695662fca8020d0effb865ad502a036bdb
dewmithmihisara <mihisaralokuhewage@>
2023-05-12 18:51:50 +0530

    machine add done

--- src/main/java/pilisaru/controller/MachineAddController.java
+++ src/main/java/pilisaru/controller/MachineAddController.java
@@ -1,4 +1,44 @@
 package pilisaru.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import pilisaru.dto.Machine;
+import pilisaru.model.MachineModel;
+
+import java.sql.SQLException;
+
 public class MachineAddController {
+    @FXML
+    private Button addBtn;
+
+    @FXML
+    private TextField machineIdTxt;
+
+    @FXML
+    private TextField machineNameTxt;
+
+    @FXML
+    void addBtnOnAction(ActionEvent event) {
+        Machine machine=new Machine(machineIdTxt.getText(),machineNameTxt.getText());
+        try {
+            boolean addMachine= MachineModel.add(machine);
+            if(addMachine){
+                System.out.println("add");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void machineIdTxtOnAction(ActionEvent event) {
+        machineNameTxt.requestFocus();
+    }
+
+    @FXML
+    void machineNameTxtOnAction(ActionEvent event) {
+        addBtn.fire();
+    }
 }
---
+++ src/main/java/pilisaru/model/MachineModel.java
@@ -0,0 +1,17 @@
+package pilisaru.model;
+
+import pilisaru.dto.Machine;
+import pilisaru.util.CrudUtil;
+
+import java.sql.SQLException;
+
+public class MachineModel {
+    public static boolean add(Machine machine) throws SQLException {
+        String sql="INSERT INTO Machine (ID,name) VALUES(?, ?)";
+        return CrudUtil.execute(
+                sql,
+                machine.getId(),
+                machine.getName()
+        );
+    }
+}
