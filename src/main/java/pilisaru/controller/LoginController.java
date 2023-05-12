597a4be6d6e3020d37562159faba9d3690b5ff14
SavindaJayasekara <thantrige32@gmail.com>
2023-05-12 11:33:52 +0530

    add menu !

--- src/main/java/pilisaru/controller/LoginController.java
+++ src/main/java/pilisaru/controller/LoginController.java
@@ -1,9 +1,17 @@
 package pilisaru.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
 
 public class LoginController {
+
+    public AnchorPane mainframethisWindow;
+    Pane registerPane;
 
     public void txtPasswordOnAction(ActionEvent event) {
     }
@@ -11,7 +19,11 @@ public class LoginController {
     public void passOnActionKeyTyped(KeyEvent keyEvent) {
     }
 
-    public void btnSingInOnAction(ActionEvent event) {
+    public void btnSingInOnAction(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/menuwindow/menuWindow.fxml"));
+        registerPane = fxmlLoader.load();
+        mainframethisWindow.getChildren().clear();
+        mainframethisWindow.getChildren().setAll(registerPane);
     }
 
     public void forgetsOnAction(ActionEvent event) {
---
+++ src/main/java/pilisaru/controller/MenuWindowController.java
@@ -0,0 +1,57 @@
+package pilisaru.controller;
+
+import javafx.event.ActionEvent;
+
+public class MenuWindowController {
+    public void btnHomeOnAction(ActionEvent event) {
+
+    }
+
+    public void btnPileOnAction(ActionEvent event) {
+    }
+
+    public void btnCompostOnAction(ActionEvent event) {
+    }
+
+    public void menuHelthUpdateOnAction(ActionEvent event) {
+    }
+
+    public void menuHelthGetInfoOnAction(ActionEvent event) {
+    }
+
+    public void btnSalaryUpdateOnAction(ActionEvent event) {
+    }
+
+    public void btnSalaryGetInfoOnAction(ActionEvent event) {
+    }
+
+    public void btnVehicalAddOnAction(ActionEvent event) {
+    }
+
+    public void btnVehicalUpdateOnAction(ActionEvent event) {
+    }
+
+    public void btnVehicalGetInfoOnAction(ActionEvent event) {
+    }
+
+    public void btnselesGetInfoOnAction(ActionEvent event) {
+    }
+
+    public void btnseleseAddDetailOnAction(ActionEvent event) {
+    }
+
+    public void btnEmployeeUpdateOnAction(ActionEvent event) {
+    }
+
+    public void btnEmployeeAddOnAction(ActionEvent event) {
+    }
+
+    public void btnEmployeeGetInfoOnAction(ActionEvent event) {
+    }
+
+    public void btnSingOutOnAction(ActionEvent event) {
+    }
+
+    public void btnInventoruOnAction(ActionEvent event) {
+    }
+}
