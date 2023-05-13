ea31ab9ec9af9f62a512dc4922e68de91c80a53c
SavindaJayasekara <thantrige32@gmail.com>
2023-05-13 09:34:52 +0530

    employee compleated !

--- src/main/java/pilisaru/controller/AddemployeeinterfaceController.java
+++ src/main/java/pilisaru/controller/AddemployeeinterfaceController.java
@@ -1,4 +1,51 @@
 package pilisaru.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import pilisaru.dto.Employee;
+import pilisaru.model.EmployeeModel;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class AddemployeeinterfaceController {
+    public ComboBox cmbNic;
+    public TextField txtEmpName;
+    public TextField txtEmpAddress;
+
+    @FXML
+    void initialize(){
+        setNicNUmbers();
+    }
+
+    private void setNicNUmbers() {
+        ObservableList nic= FXCollections.observableArrayList();
+        try {
+            List<String> nics=EmployeeModel.getNicNumbers();
+
+            for(String s : nics){
+                nic.add(s);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        cmbNic.setItems(nic);
+    }
+
+
+    public void cmbNicOnAction(ActionEvent event) {
+        String nic = (String) cmbNic.getValue();
+
+        try {
+            Employee employee= EmployeeModel.getAll(nic);
+            txtEmpName.setText(employee.getName());
+            txtEmpAddress.setText(employee.getAddress());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
