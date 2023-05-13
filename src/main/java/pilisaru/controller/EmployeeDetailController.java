f7e694c0995f09511c8bb7600ce1eb7d7d204bed
SavindaJayasekara <thantrige32@gmail.com>
2023-05-13 10:27:41 +0530

    add employee nic nmbers from health !

--- src/main/java/pilisaru/controller/EmployeeDetailController.java
+++ src/main/java/pilisaru/controller/EmployeeDetailController.java
@@ -2,24 +2,21 @@ package pilisaru.controller;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import pilisaru.model.EmployeeModel;
 
 import java.sql.SQLException;
 import java.util.List;
 
-public class employeeFormController {
-    public ComboBox cmbEmp;
-    public Button updatebtn;
+public class EmployeeDetailController {
+    public ComboBox cmbYear;
+    public ComboBox cmbEmpNic;
 
     @FXML
     void initialize(){
-        setNicNUmbers();
-    }
 
+    }
     private void setNicNUmbers() {
         ObservableList nic= FXCollections.observableArrayList();
         try {
@@ -31,10 +28,6 @@ public class employeeFormController {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        cmbEmp.setItems(nic);
-    }
-
-    public void buttonPressed(ActionEvent event) {
-
+        cmbEmpNic.setItems(nic);
     }
 }
