b066ad46c7bbe4270bd6db6e3edf7257082c23fd
SavindaJayasekara <thantrige32@gmail.com>
2023-07-05 19:41:53 +0530

    modify vehicle add form

--- src/main/java/pilisaru/controller/HomeWindowController.java
+++ src/main/java/pilisaru/controller/HomeWindowController.java
@@ -86,6 +86,6 @@ public class HomeWindowController {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        initialize();
+//        initialize();
     }
 }
--- src/main/java/pilisaru/controller/addVehicleController.java
+++ src/main/java/pilisaru/controller/addVehicleController.java
@@ -1,4 +1,5 @@
 package pilisaru.controller;
 
 public class addVehicleController {
+
 }
