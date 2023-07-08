39f2a97830c83ee35cb109b72e75378bf757aaf8
SavindaJayasekara <thantrige32@gmail.com>
2023-07-08 15:25:45 +0530

    odify update

--- src/main/java/pilisaru/controller/updateController.java
+++ src/main/java/pilisaru/controller/updateController.java
@@ -1,4 +1,22 @@
 package pilisaru.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class updateController {
+    public ChoiceBox txtWeight;
+    public TextField txtCouncil;
+    public Button btnUpdate;
+    public ComboBox cmbVehicleNum;
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void cmbVehicleNumOnAction(ActionEvent actionEvent) {
+
+    }
 }
